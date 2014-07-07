abb326d7ac49488ac246481c033ad04dcc3bda8f
Suraj Dubey <sdubey.dubey14@gmail.com>
2014-07-07 20:18:54 +0530

    Update: Twitter Login

--- src/org/codelearn/twitter/TwitterConstants.java
+++ src/org/codelearn/twitter/TwitterConstants.java
@@ -3,7 +3,22 @@ package org.codelearn.twitter;
 public class TwitterConstants {
 	
 	public static final String SHARED_PREFERENCE = "codelearn_twitter";
-	public static final String CONSUMER_KEY = "pC3D9eMxb8pXNAPhBHAQFYLoS";
+	
+	/*public static final String CONSUMER_KEY = "pC3D9eMxb8pXNAPhBHAQFYLoS";
 	public static final String CONSUMER_SECRET = "8qhOLjLHiqLQJLLQUnBNSlnUoFtbLHjWQlnaxS5W3n6NkkUeOi";
+	*/
+	
+	public static final String CONSUMER_KEY = "ptlTdGpcmjwIDAaaFxfnJBZqu";
+	public static final String CONSUMER_SECRET = "k6zDhMlYuWlYBQVMmv18Tr9GmuVsK0Qq5RoVLCsOSpFrFuEyeA";
+	
+	
+	public static final String CALLBACK_URL = "oauth://t4jsample";
+	
+	static final String IEXTRA_AUTH_URL = "auth_url";
+	static final String IEXTRA_OAUTH_VERIFIER = "oauth_verifier";
+	static final String IEXTRA_OAUTH_TOKEN = "oauth_token";
+	
+	static final String PREF_KEY_SECRET = "oauth_token_secret";
+	static final String PREF_KEY_TOKEN = "oauth_token";
 
 }
